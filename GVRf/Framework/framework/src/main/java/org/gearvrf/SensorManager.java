9239e5000fa88f30b5b68d1497888427c1794363
Jonathan Reynolds <j14reynolds@gmail.com>
2017-07-14 09:18:09 -0700

    Update 3DCursorLibrary and framework

--- GVRf/Framework/framework/src/main/java/org/gearvrf/SensorManager.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/SensorManager.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.gearvrf.GVRCursorController.ActiveState;
+import org.gearvrf.utility.Log;
 import org.joml.Vector3f;
 
 /**
