28ce1b4eb863971e565a3ea82636be0a333c0ead
Jonathan Reynolds <j14reynolds@gmail.com>
2017-07-17 10:50:13 -0700

    Move static mesh generation function to GVRMesh

--- GVRf/Framework/framework/src/main/java/org/gearvrf/scene_objects/GVRGUISceneObject.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/scene_objects/GVRGUISceneObject.java
@@ -51,16 +51,45 @@ public class GVRGUISceneObject extends GVRViewSceneObject {
     /**
      * Constructor for GVRGUISceneObject
      *
-     * This constructor will generate a {@link GVRMesh} for you. The curved parameter specifies
-     * whether the generated mesh should be planar or curved.
+     * This constructor will generate a planar {@link GVRMesh} for you.
      *
      * @param gvrContext    current {@link GVRContext}
      * @param gvrView       the {@link GVRView} to be displayed on the GVRGUISceneObject
-     * @param curved        indicates what kind of mesh will be generated for the GVRView
      */
-    public <T extends View & GVRView> GVRGUISceneObject(GVRContext gvrContext, T gvrView, boolean curved) {
-        this(gvrContext, gvrView, generateMesh(gvrContext, gvrView, curved));
+    public <T extends View & GVRView> GVRGUISceneObject(GVRContext gvrContext, T gvrView) {
+        this(gvrContext, gvrView, planarMesh(gvrContext, gvrView));
     }
+
+    /**
+     * Constructor for GVRGUISceneObject
+     *
+     * This constructor will generate a curved {@link GVRMesh} for you based on the
+     * arguments passed.
+     *
+     * @param gvrContext    current {@link GVRContext}
+     * @param gvrView       the {@link GVRView} to be displayed on the GVRGUISceneObject
+     * @param radius        the radius of the circle to use to create the arc
+     * @param centralAngle  the central angle of the arc
+     */
+    public <T extends View & GVRView> GVRGUISceneObject(GVRContext gvrContext, T gvrView, float radius, float centralAngle) {
+        this(gvrContext, gvrView, curvedMesh(gvrContext, gvrView, radius, centralAngle));
+    }
+
+    /**
+     * Constructor for GVRGUISceneObject
+     *
+     * This constructor will generate a curved {@link GVRMesh} for you based on the
+     * arguments passed. The central angle of the arc of the curve will be 45 degrees.
+     *
+     * @param gvrContext    current {@link GVRContext}
+     * @param gvrView       the {@link GVRView} to be displayed on the GVRGUISceneObject
+     * @param radius        the radius of the circle to use to create the arc
+     */
+    public <T extends View & GVRView> GVRGUISceneObject(GVRContext gvrContext, T gvrView, float radius) {
+        this(gvrContext, gvrView, curvedMesh(gvrContext, gvrView, radius, 45f));
+    }
+
+
 
     /**
      * Constructor for GVRGUISceneObject
@@ -90,126 +119,19 @@ public class GVRGUISceneObject extends GVRViewSceneObject {
         this.setSensor(new GVRBaseSensor(gvrContext));
     }
 
-    /**
-     * Generates a mesh based on the arguments passed. To standardize the
-     * output mesh dimensions, we make the longest side of the view 1 units
-     * in length
-     *
-     * @param gvrContext    the current {@link GVRContext}
-     * @param gvrView       the {@link GVRView} to be displayed on the GVRGUISceneObject
-     * @param curved        indicates what kind of mesh will be generated for the GVRView
-     */
-    private static GVRMesh generateMesh(GVRContext gvrContext, GVRView gvrView, boolean curved){
+    private static GVRMesh planarMesh(GVRContext gvrContext, GVRView gvrView){
         View view = gvrView.getView();
         int w = view.getWidth();
         int h = view.getHeight();
-
-        if (curved){
-            return generateCurvedMesh(gvrContext, w, h);
-        }
-        else {
-            int largest = w > h ? w : h;
-            return gvrContext.createQuad((float)w/largest*1.0f, (float)h/largest*1.0f);
-        }
-
+        int largest = w > h ? w : h;
+        return gvrContext.createQuad((float)w/largest*1.0f, (float)h/largest*1.0f);
     }
 
-    /**
-     * Generates a curved mesh based on the width and height of a {@link GVRView}
-     * @param gvrContext    current {@link GVRContext}
-     * @param width         the width of a GVRView
-     * @param height        the height of a GVRView
-     * @return
-     */
-    private static GVRMesh generateCurvedMesh(GVRContext gvrContext, int width, int height){
-        GVRMesh mesh = new GVRMesh(gvrContext);
-
-        double ratio = (double)width/height;
-        // Scale the number of subdivisions with the width of the view relative to its height
-        int subdivisions = (int) Math.ceil(ratio*SUBDIVISION_MUTLIPLIER);
-        // Let each subdivision represent a constant number of degrees on the arc
-        int degrees = subdivisions*DEGREES_PER_SUBDIVISION;
-        double startDegree = -degrees/2.0;
-        double radius, h;
-        // Choose a height and radius that ensure the mesh is at most 1 unit tall
-        // or 1 unit wide (along the circumference)
-        if(width > height) {
-            radius = 1.0 / Math.toRadians(degrees);
-            h = 1.0 / ratio;
-        }
-        else {
-            h = 1.0;
-            radius = ratio / Math.toRadians(degrees);
-        }
-
-        float yTop = (float)h/2;
-        float yBottom = -yTop;
-
-        float[] vertices = new float[(subdivisions+1)*6];
-        float[] normals = new float[(subdivisions+1)*6];
-        float[] texCoords= new float[(subdivisions+1)*4];
-        char[] triangles = new char[subdivisions*6];
-
-        /*
-         * The following diagram illustrates the construction method
-         * Let s be the number of subdivisions, then we create s pairs of vertices
-         * like so
-         *
-         * {0}  {2}  {4} ... {2s-1}
-         *                             |y+
-         * {1}  {3}  {5} ... {2s}      |___x+
-         *                          z+/
-         */
-        for(int i = 0; i <= subdivisions; i++){
-            double angle = Math.toRadians(-90+startDegree + DEGREES_PER_SUBDIVISION*i);
-            double cos = Math.cos(angle);
-            double sin = Math.sin(angle);
-            float x = (float) (radius * cos);
-            float z = (float) ((radius * sin) + radius);
-            vertices[6*i] = x;
-            vertices[6*i + 1] = yTop;
-            vertices[6*i + 2] = z;
-            normals[6*i] = (float)-cos;
-            normals[6*i + 1] = 0.0f;
-            normals[6*i + 2] = (float)-sin;
-            texCoords[4*i] = (float)i/subdivisions;
-            texCoords[4*i + 1] = 0.0f;
-
-            vertices[6*i + 3] = x;
-            vertices[6*i + 4] = yBottom;
-            vertices[6*i + 5] = z;
-            normals[6*i + 3] = (float)-cos;
-            normals[6*i + 4] = 0.0f;
-            normals[6*i + 5] = (float)-sin;
-            texCoords[4*i + 2] = (float)i/subdivisions;
-            texCoords[4*i + 3] = 1.0f;
-        }
-
-        /*
-         * Referring to the diagram above, we create two triangles
-         * for each pair of consecutive pairs of vertices
-         * (e.g. we create two triangles with {0, 1} and {2, 3}
-         *  and two triangles with {2, 3} and {4, 5})
-         *
-         * {0}--{2}--{4}-...-{2s-1}
-         *  | ＼  | ＼ |        |       |y+
-         * {1}--{3}--{5}-...-{2s}      |___x+
-         *                          z+/
-         */
-        for(int i = 0; i < subdivisions; i++){
-            triangles[6*i] = (char)(2*(i+1)+1);
-            triangles[6*i+1] = (char) (2*(i));
-            triangles[6*i+2] = (char) (2*(i)+1);
-            triangles[6*i+3] = (char) (2*(i+1)+1);
-            triangles[6*i+4] = (char) (2*(i+1));
-            triangles[6*i+5] = (char) (2*(i));
-        }
-
-        mesh.setVertices(vertices);
-        mesh.setNormals(normals);
-        mesh.setTexCoords(texCoords);
-        mesh.setIndices(triangles);
-        return mesh;
+    private static GVRMesh curvedMesh(GVRContext gvrContext, GVRView gvrView, float radius, float centralAngle){
+        View view = gvrView.getView();
+        int w = view.getWidth();
+        int h = view.getHeight();
+        return GVRMesh.createCurvedMesh(gvrContext, w, h, centralAngle, radius);
     }
 
     private ISensorEvents GUIEventListener = new ISensorEvents() {
